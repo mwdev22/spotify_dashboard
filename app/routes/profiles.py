2a1da74aab1c6827f512fc9c05763b21e50c5584
macwojDev <92087346+macwojDev@users.noreply.github.com>
2024-01-03 12:12:21 +0100

    zaczecie pracy nad playlistsami

--- app/routes/profiles.py
+++ app/routes/profiles.py
@@ -15,8 +15,6 @@ def user_profile():
     check_token(session)   # checking if user is logged in
     refresh(session)   # refreshing token if expired
 
-
-
     headers = {
         'Authorization':f"Bearer {session['access_token']}"
     }
@@ -24,13 +22,13 @@ def user_profile():
     prof_response = get(f'{API_BASE_URL}me', headers=headers)
     profile = prof_response.json()
 
-    # adding user to database if logging for the first time
+    # searching if user exists in our database
     existing_user = User.query.filter_by(spotify_id=profile['id']).first()
 
     if not existing_user:
 
-        new_user = User(
-            id=User.count_users(),
+        new_user = User( 
+            id=User.count_users(), # user ids based on number of users
             spotify_id=profile['id'],
             display_name=profile['display_name'],
             email=profile['email'],
@@ -38,10 +36,17 @@ def user_profile():
             followers=profile['followers']['total']
         )
 
-        # Add the new user to the database
+        # add the new user to the database
         db.session.add(new_user)
         db.session.commit()
-
+    else:
+        # updating user data if not logged first time
+        existing_user.display_name = profile['display_name']
+        existing_user.email = profile['email']
+        existing_user.subscription = profile['product']
+        existing_user.followers = profile['followers']['total']
+        db.session.commit()
+        
     print(User.count_users())
     print(User.average_followers())
 
@@ -51,7 +56,7 @@ def user_profile():
         'offset':0
     }
 
-    # Update the endpoint to include the correct time_range parameter
+    # getting top tracks and artists including params
     artists_response = get(f'{API_BASE_URL}me/top/artists?{urlencode(params)}', headers=headers)
     artists = artists_response.json()
 
@@ -70,6 +75,7 @@ def my_playlists():
         'Authorization':f"Bearer {session['access_token']}"
     }
     response = get(f'{API_BASE_URL}me/playlists', headers=headers)
-    playlists = response.json()
-
+    playlists = response.json()['items']
+    for playlist in playlists:
+        print(playlist['name'])
     return jsonify(playlists)
