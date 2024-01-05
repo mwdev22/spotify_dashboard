70d2468c1dcfb179297d658cdf61dcbf5dbca81e
macwojDev <92087346+macwojDev@users.noreply.github.com>
2024-01-05 18:14:12 +0100

    wstępnie skończony projekt, udostępnienie .env, do dalszego rozwoju jeśli będę miał fajny pomysł

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from app.routes.profiles import profile_bp
 from app.routes.auth import auth_bp
 from app.routes.main import main_bp
 from app.routes.trends import trends_bp
-from app.routes.search import search_bp
 
 from app.utils.extenisons import db, migrate
 
@@ -17,9 +16,9 @@ def create_app():
     app.register_blueprint(auth_bp)
     app.register_blueprint(profile_bp)
     app.register_blueprint(trends_bp)
-    app.register_blueprint(search_bp)
 
 
+    # db initialization
     db.init_app(app)
     migrate.init_app(app, db)
 
--- app/routes/main.py
+++ app/routes/main.py
@@ -8,3 +8,7 @@ def index():
     user_count = User.count_users()
     follows_average = User.average_followers()
     return render_template('index.html', user_count=user_count, follows_average=follows_average)
+
+@main_bp.route('/privacy')
+def privacy():
+    return render_template('privacy.html')
